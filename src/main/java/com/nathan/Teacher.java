5b2fd346d8c0f9115fcec6e73bd25de1d4b1fa87
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-10 16:51:08 +0300

    Feat: teacher class

--- src/main/java/com/nathan/Teacher.java
+++ src/main/java/com/nathan/Teacher.java
@@ -1,5 +1,17 @@
 package com.nathan;
 
-public class Teacher {
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+@Getter
+@Setter
+public class Teacher extends Person {
+
+    private String speciality;
+
+    public Teacher(int id, String firstName, String lastName, LocalDate dateOfBirth, String email, String phoneNumber, String speciality) {
+        super(id, firstName, lastName, dateOfBirth, email, phoneNumber);
+        this.speciality = speciality;
+    }
 }
