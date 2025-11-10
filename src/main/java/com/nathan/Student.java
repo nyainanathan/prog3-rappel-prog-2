88cef34aaf9e5b73324657d1c5d7568066010ff0
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-10 17:08:12 +0300

    Feat: link between student and tutor as attribute field

--- src/main/java/com/nathan/Student.java
+++ src/main/java/com/nathan/Student.java
@@ -10,10 +10,16 @@ import java.time.LocalDate;
 @Setter
 public class Student extends Person{
     private String group;
+    private Tutor tutor;
+
+    public Student(int id, String firstName, String lastName, LocalDate dateOfBirth, String email, String phoneNumber, String group, Tutor tutor) {
+        super(id, firstName, lastName, dateOfBirth, email, phoneNumber);
+        this.group = group;
+        this.tutor = tutor;
+    }
 
     public Student(int id, String firstName, String lastName, LocalDate dateOfBirth, String email, String phoneNumber, String group) {
         super(id, firstName, lastName, dateOfBirth, email, phoneNumber);
         this.group = group;
     }
-    //private Tutor tutor;
 }
